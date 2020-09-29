bd80fe9a9eb1e0c480fd4b924c735cf7b8d3c321
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-09-29 11:44:44 +0300

    Added missing packages

--- CleanCode/FullRefactoring/PostRepository.cs
+++ CleanCode/FullRefactoring/PostRepository.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using Project.UserControls;
 
 namespace CleanCode.FullRefactoring
 {
--- CleanCode/LongMethods/Example1.cs
+++ CleanCode/LongMethods/Example1.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.IO;
-using System.Configuration;
 using System.Data.SqlClient;
 using System.Data;
 
